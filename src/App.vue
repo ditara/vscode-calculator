4861986f5ee5854282d7359e213f91f11acfa385
unknown <dhitta28.dr@mail.com>
2020-02-04 10:28:18 +0700

    test

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div id="app">
+    <h1 style="color: #f1e867; font-size: 50px">Calculator</h1>
+    <Calculator/>
+  </div>
+</template>
+
+<script>
+import Calculator from './components/Calculator.vue'
+
+export default {
+  name: 'app',
+  components: {
+    Calculator
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #4d4b3c;
+  margin-top: 60px;
+}
+</style>
